1db3cf75e943004a66258b9a7d61c95670e6fd75
Masa <masa@MacBook-Air-Masa.local>
2024-05-23 00:19:16 +0200

    ApiResponse model replaced in exceptionHandler

--- poorbook-backend/models/handlers/exceptionHandler.py
+++ poorbook-backend/models/handlers/exceptionHandler.py
@@ -3,7 +3,6 @@ sys.path.append('.')
 
 from fastapi.exceptions import RequestValidationError
 from starlette.requests import Request
-from starlette.responses import JSONResponse
 from starlette import status
 from models.responses.apiResponse import ApiResponse
 from models.exceptions.invalidApiKeyException import InvalidApiKeyException
@@ -12,17 +11,14 @@ from models.exceptions.missingApiKeyException import MissingApiKeyException
 
 def handleValidationError(request: Request, exception: RequestValidationError):
     """ Handle RequestValidationError from pydantic """
-    response = ApiResponse.createErrorResponse(exception)
-    return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, content=response.model_dump())
+    return ApiResponse.createResponse().asError(exception, status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-def handleApiKeyValidation(request: Request, exc: InvalidApiKeyException):
+
+def handleApiKeyValidation(request: Request, exception: InvalidApiKeyException):
     """ Handle InvalidApiKey exception """
-    statusCode = status.HTTP_401_UNAUTHORIZED
-    response = ApiResponse.createCustomErrorRespomse(statusCode=statusCode, exception=exc)
-    return JSONResponse(status_code=statusCode, content=response.model_dump())
+    return ApiResponse.createResponse().asError(exception, status.HTTP_401_UNAUTHORIZED)
 
-def handleMissingApiKeyValidation(request: Request, exc: MissingApiKeyException):
+
+def handleMissingApiKeyValidation(request: Request, exception: MissingApiKeyException):
     """ Handle MissinApiKey exception """
-    statusCode = status.HTTP_403_FORBIDDEN
-    response = ApiResponse.createCustomErrorRespomse(statusCode=statusCode, exception=exc)
-    return JSONResponse(status_code=statusCode, content=response.model_dump())
+    return ApiResponse.createResponse().asError(exception, status.HTTP_403_FORBIDDEN)
