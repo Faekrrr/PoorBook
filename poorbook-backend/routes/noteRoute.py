96a21e1b8ea3a41708155a6ca38a15d5e233cacc
Masa <masa@MacBook-Air-Masa.local>
2024-06-08 23:57:24 +0200

    Added note router

---
+++ poorbook-backend/routes/noteRoute.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, Depends, Query, status
+from models.responses.apiResponse import ApiResponse
+from models.entities.note import Note
+from data.noteRepository import NoteRepository
+from models.exceptions.apiExceptions import ItemNotFoundException, ItemNotCreatedException, ItemNotDeletedException, ItemNotUpdatedException
+from models.app.getCondition import GetCondition
+
+
+noteRouter = APIRouter()
+
+@noteRouter.post("/notes", response_model=ApiResponse,
+                summary="Create new note.",
+                dependencies="Create new note using Note model.")
+def insertNote(newNote: Note, repository: NoteRepository = Depends()):
+    """ Creates new note in collection """
+    result = repository.insert(newNote)
+
+    if not result:
+        raise ItemNotCreatedException("Note hasnt been created.")
+   
+    return ApiResponse.createResponse().asSuccess(status.HTTP_201_CREATED)
+
+@noteRouter.get("/notes", response_model=ApiResponse,
+               summary="Get all notes.",
+               description="Get all notes with pagination.")
+def getNotes(offset: int = Query(0, description="How much to skip"),
+                take: int = Query(10, description="How much to take"),
+                order: str = Query("ASC", description="How to order"),
+                repository: NoteRepository = Depends()):
+    """ Gets all notes from collection """
+    result = repository.getSorted(GetCondition(
+        take=take,
+        offset=offset,
+        sortOrder=order,
+        sortBy="noteCreated"
+    ))
+    
+    if not result:
+        raise ItemNotFoundException("No notes found.")
+
+    return ApiResponse.createResponse().addContent(result).asSuccess(status.HTTP_200_OK)
+
+
+@noteRouter.delete("/notes/{id}", response_model=ApiResponse,
+                  status_code=status.HTTP_204_NO_CONTENT,
+                  summary="Delete note by Id.",
+                  description="Delete note with provided Id.")
+def deleteNote(id: str, repository: NoteRepository = Depends()):
+    """ Deletes note by Id """
+    result = repository.delete(id)
+    if not result:
+        raise ItemNotDeletedException("Note hasnt been deleted")
+    
+@noteRouter.put("/notes/{id}",
+                summary="Update note by Id",
+                description="Update note of provided Id with provided content",
+                status_code=status.HTTP_204_NO_CONTENT)
+async def updateTask(id: str, changes: Note, repository: NoteRepository = Depends()):
+    """ Update note by Id """
+    result = repository.update(id, changes)
+
+    if not result:
+        raise ItemNotUpdatedException("Note hasnt been updated")
