81dd3fc62a13bf3f40deff970b99a0526ccfbe5d
Masa <masa@MacBook-Air-Masa.local>
2024-06-09 20:19:45 +0200

    Fixed dependencies and docs for note route

--- poorbook-backend/routes/eventRoute.py
+++ poorbook-backend/routes/eventRoute.py
@@ -9,7 +9,7 @@ from models.exceptions.apiExceptions import ItemNotFoundException, ItemNotCreate
 
 eventRouter = APIRouter()
 
-@eventRouter.post("/events", status_code=status.HTTP_201_CREATED, response_model= ApiResponse)
+@eventRouter.post("/events", status_code=status.HTTP_201_CREATED, response_model= ApiResponse, tags=["Events"])
 async def insertEvent(newEvent: Event, repository: EventRepository = Depends()):
     """ Create new event"""  
     result = repository.insert(newEvent)
@@ -20,7 +20,7 @@ async def insertEvent(newEvent: Event, repository: EventRepository = Depends()):
     return ApiResponse.createResponse().asSuccess(status.HTTP_201_CREATED)
         
     
-@eventRouter.post("/events", status_code=status.HTTP_200_OK, response_model= ApiResponse)
+@eventRouter.post("/events", status_code=status.HTTP_200_OK, response_model= ApiResponse, tags=["Events"])
 async def getEventByDate(condition: Optional[Dict[str, Any]], 
                          offset: int = Query(0, description="How much to skip"),
                          take: int = Query(10, description="How much to take"),
@@ -33,7 +33,7 @@ async def getEventByDate(condition: Optional[Dict[str, Any]],
     return ApiResponse.createResponse().addContent(result).asSuccess(status.HTTP_200_OK)
     
 
-@eventRouter.delete("/events/{id}")
+@eventRouter.delete("/events/{id}", tags=["Events"])
 async def deleteTask(id: str, repository: EventRepository = Depends()):
     """ Delete event by Id """
     result = repository.delete(id)
@@ -42,7 +42,7 @@ async def deleteTask(id: str, repository: EventRepository = Depends()):
         raise ItemNotDeletedException("Event hasnt been deleted")
     
     
-@eventRouter.put("/events/{id}")
+@eventRouter.put("/events/{id}", tags=["Events"])
 async def updateTask(id: str, changes: Event, repository: EventRepository = Depends()):
     """ Update event by Id """
     result = repository.update(id, changes)
--- poorbook-backend/routes/noteRoute.py
+++ poorbook-backend/routes/noteRoute.py
@@ -10,19 +10,21 @@ noteRouter = APIRouter()
 
 @noteRouter.post("/notes", response_model=ApiResponse,
                 summary="Create new note.",
-                dependencies="Create new note using Note model.")
-def insertNote(newNote: Note, repository: NoteRepository = Depends()):
+                description="Create a new note using the Note model.",
+                tags=["Notes"])
+async def insertNote(newNote: Note, repository: NoteRepository = Depends()):
     """ Creates new note in collection """
     result = repository.insert(newNote)
 
     if not result:
-        raise ItemNotCreatedException("Note hasnt been created.")
+        raise ItemNotCreatedException("Note hasn't been created.")
    
     return ApiResponse.createResponse().asSuccess(status.HTTP_201_CREATED)
 
 @noteRouter.get("/notes", response_model=ApiResponse,
                summary="Get all notes.",
-               description="Get all notes with pagination.")
+               description="Get all notes with pagination.",
+               tags=["Notes"])
 def getNotes(offset: int = Query(0, description="How much to skip"),
                 take: int = Query(10, description="How much to take"),
                 order: str = Query("ASC", description="How to order"),
@@ -41,10 +43,10 @@ def getNotes(offset: int = Query(0, description="How much to skip"),
     return ApiResponse.createResponse().addContent(result).asSuccess(status.HTTP_200_OK)
 
 
-@noteRouter.delete("/notes/{id}", response_model=ApiResponse,
-                  status_code=status.HTTP_204_NO_CONTENT,
+@noteRouter.delete("/notes/{id}", status_code=status.HTTP_204_NO_CONTENT,
                   summary="Delete note by Id.",
-                  description="Delete note with provided Id.")
+                  description="Delete note with provided Id.",
+                  tags=["Notes"])
 def deleteNote(id: str, repository: NoteRepository = Depends()):
     """ Deletes note by Id """
     result = repository.delete(id)
@@ -52,10 +54,11 @@ def deleteNote(id: str, repository: NoteRepository = Depends()):
         raise ItemNotDeletedException("Note hasnt been deleted")
     
 @noteRouter.put("/notes/{id}",
-                summary="Update note by Id",
-                description="Update note of provided Id with provided content",
-                status_code=status.HTTP_204_NO_CONTENT)
-async def updateTask(id: str, changes: Note, repository: NoteRepository = Depends()):
+                summary="Update note by Id.",
+                description="Update note of provided Id with provided content.",
+                status_code=status.HTTP_204_NO_CONTENT,
+                tags=["Notes"])
+async def updateNote(id: str, changes: Note, repository: NoteRepository = Depends()):
     """ Update note by Id """
     result = repository.update(id, changes)
 
